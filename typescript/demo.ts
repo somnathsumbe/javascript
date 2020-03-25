d75573206518efd46172da38db81a942f38be0af
sumbes <p10475242@amtsybexad.onmicrosft.com>
2020-03-25 18:27:46 +0530

    all new pages

---
+++ typescript/demo.ts
@@ -0,0 +1,21 @@
+var str:string="somnath sumbe";
+console.log(str);
+console.log(typeof(str));
+
+var str1="sunita";
+console.log(str1);
+console.log(typeof(str1));
+
+var result:string=`${str} hi how are you ${str1}`;
+console.log(result);
+console.log(typeof(result));
+var isactive:boolean=true;
+
+console.log(isactive);
+console.log(typeof(isactive));
+var isupdate:boolean=false;
+
+console.log(isupdate);
+console.log(typeof(isupdate));
+
+
